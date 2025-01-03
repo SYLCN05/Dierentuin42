4eec5aa50e53a89938ce2586cb4154f0dc01870e
SYLCN05 <Siyaryalcin25@gmail.com>
2025-01-03 22:18:59 +0100

    Home index view , added a new Footer partial view

--- Dierentuin42/Controllers/HomeController.cs
+++ Dierentuin42/Controllers/HomeController.cs
@@ -27,5 +27,10 @@ namespace Dierentuin42.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Footer() 
+        {
+            return PartialView("_Footer");
+        }
     }
 }
